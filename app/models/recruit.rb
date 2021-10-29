21d19f233e08126efe02115b6b31d8565ec9643a
yuto-kawahara <mashroom1384@gmail.com>
2021-10-29 14:18:51 +0000

    [Add]recruitとuserのアソシエーションとバリデーションを追加(途中)

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -1,2 +1,16 @@
 class Recruit < ApplicationRecord
+  belongs_to :user
+  belongs_to :play_form
+  belongs_to :entry_condition
+  has_many   :reserves,         dependent: :destroy
+  has_many   :recruit_comments, dependent: :destroy
+  has_many   :notifications,    dependent: :destroy
+
+  attachment :image
+  validates :title,               presence: true
+  validates :hold_datetime,       presence: true
+  validates :capacity,            presence: true
+  validates :explanation,         presence: true
+  validates :discord_server_link, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recruits,              dependent: :destroy
+  has_many :recruit_comments,      dependent: :destroy
+  has_many :reserves,              dependent: :destroy
+  has_many :messages,              dependent: :destroy
+  has_many :user_rooms,            dependent: :destroy
+  has_many :discord_server_links,  dependent: :destroy
+
+  has_many :following,             class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed,              class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user,        through: :following, source: :followed
+  has_many :follower_user,         through: :followed,  source: :following
+
+  has_many :active_notifications,  class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+
+  attachment :icon_image
+  validates :nickname,     presence: true, uniqueness: true
 end
